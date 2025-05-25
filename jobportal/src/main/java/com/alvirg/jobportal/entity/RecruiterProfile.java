cd6a84924f00941359f0303a85a1f592f5e443b0
albertegi <albertegi90@gmail.com>
2025-05-26 00:28:52 +0100

    updated Users and UsersType Entities and created RecruiterProfile Entity

---
+++ jobportal/src/main/java/com/alvirg/jobportal/entity/RecruiterProfile.java
@@ -0,0 +1,17 @@
+//package com.alvirg.jobportal.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "recruiter_profile")
+//public class RecruiterProfile {
+//
+//    private int userAccountId;
+//
+//    private Users userId;
+//
+//    private String firstName;
+//    private String lastName;
+//    private String city;
+//}
