13d249c5291aa63397efa17119231968dc331e63
albertegi <albertegi90@gmail.com>
2025-05-30 23:23:06 +0100

    created RecruitmentProfileRepository

--- jobportal/src/main/java/com/alvirg/jobportal/entity/Skills.java
+++ jobportal/src/main/java/com/alvirg/jobportal/entity/Skills.java
@@ -1,9 +1,72 @@
 package com.alvirg.jobportal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "skills")
 public class Skills {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String experienceLevel;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job_seeker_profile")
+    private JobSeekerProfile jobSeekerProfile;
+
+    public Skills() {
+    }
+
+    public Skills(int id, String name, String experienceLevel, JobSeekerProfile jobSeekerProfile) {
+        this.id = id;
+        this.name = name;
+        this.experienceLevel = experienceLevel;
+        this.jobSeekerProfile = jobSeekerProfile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public void setExperienceLevel(String experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+
+    public JobSeekerProfile getJobSeekerProfile() {
+        return jobSeekerProfile;
+    }
+
+    public void setJobSeekerProfile(JobSeekerProfile jobSeekerProfile) {
+        this.jobSeekerProfile = jobSeekerProfile;
+    }
+
+    @Override
+    public String toString() {
+        return "Skills{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", experienceLevel='" + experienceLevel + '\'' +
+                ", jobSeekerProfile=" + jobSeekerProfile +
+                '}';
+    }
 }
---
+++ jobportal/src/main/java/com/alvirg/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,9 @@
+package com.alvirg.jobportal.repository;
+
+import com.alvirg.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+
+}
